23678d081007134d89acd6104fcc4507bee64aa1
muscleman <musclesonvacation@hotmail.com>
2020-02-22 16:35:29 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let zmq = require("zeromq"),
+subscriber = zmq.socket("sub");
+
+subscriber.connect("tcp://127.0.0.1:3000");
+subscriber.subscribe("");
+console.log("Subscriber connected to port 3000");
+
+subscriber.on("message", function(topic) {
+  console.log(
+    "received a message related to:",
+    topic.toString('hex'));
+});
+
+process.on('SIGINT', () => {
+    subscriber.close()
+    console.log('\nClosed')
+})
